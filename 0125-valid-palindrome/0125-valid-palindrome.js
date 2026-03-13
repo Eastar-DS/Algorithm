fe2fc3762c07d40cca3fd2ac55704c4e6725a411
Eastar-DS <72285245+Eastar-DS@users.noreply.github.com>
2026-03-14 00:18:21 +0900

    Time: 7 ms (38.37%), Space: 61.1 MB (23.36%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,10 +3,7 @@
  * @return {boolean}
  */
 var isPalindrome = function (s) {
-    let asciis = [...s].map((char) => {
-        let ascii = char.charCodeAt(0);
-        return (ascii >= 65 && ascii <= 90) ? ascii + 32 : ascii;
-    }).filter((ascii) => (ascii >= 97 && ascii <= 122) || (ascii >= 48 && ascii <= 57));
+    const asciis = [...s.toLowerCase()].map((char) => char.charCodeAt(0)).filter((ascii) => (ascii >= 97 && ascii <= 122) || (ascii >= 48 && ascii <= 57));
 
     const length = asciis.length;
     for (let i = 0; i < parseInt(asciis.length / 2); i++) {
