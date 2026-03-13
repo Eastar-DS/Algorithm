6b4717a5d8b786b6d67d457060448dd281ae50bb
Eastar-DS <72285245+Eastar-DS@users.noreply.github.com>
2026-03-14 00:33:30 +0900

    Time: 6 ms (51.74%), Space: 55.7 MB (95.92%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -4,10 +4,11 @@
  */
 var isPalindrome = function (s) {
     const onlyLetters = s.replace(/[^a-z0-9]/gi, "").toLowerCase();
-    
-    if (onlyLetters === onlyLetters.split("").reverse().join("")) {
-        return true;
-    }
-    return false;
 
+    for (let i = 0; i < parseInt(onlyLetters.length / 2); i++) {
+        if (onlyLetters[i] != onlyLetters[onlyLetters.length - i - 1]) {
+            return false;
+        }
+    }
+    return true;
 };
