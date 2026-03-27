b97c1290bcdf32c21fa6cabef4ae4d01b9f75380
Eastar-DS <72285245+Eastar-DS@users.noreply.github.com>
2026-03-27 22:22:50 +0900

    Time: 1 ms (99.58%), Space: 69.2 MB (78.73%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.ts
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.ts
@@ -1,15 +1,15 @@
 function findMaxAverage(nums: number[], k: number): number {
   let result = 0;
   for (let i = 0; i < k; i++) {
-    result += nums[i] / k;
+    result += nums[i];
   };
 
   let sum = result;
 
   for (let i = k; i < nums.length; i++) {
-    sum = sum + (nums[i] - nums[i-k]) / k ;
+    sum = sum + (nums[i] - nums[i-k]);
     result = Math.max(result, sum);
   };
 
-  return result;
+  return result / k;
 };
