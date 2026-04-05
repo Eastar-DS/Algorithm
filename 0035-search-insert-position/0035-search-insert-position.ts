5b74c721a5a4df7dfe104c181357f1deb84902db
Eastar-DS <72285245+Eastar-DS@users.noreply.github.com>
2026-04-05 19:53:43 +0900

    Time: 0 ms (100%), Space: 55.3 MB (70.6%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.ts
@@ -0,0 +1,20 @@
+function searchInsert(nums: number[], target: number): number {
+    let left = 0;
+    let right = nums.length - 1;
+    while (left < right) {
+        const mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    if (nums[left] >= target) {
+        return left;
+    } else {
+        return left + 1;
+    }
+};
