7c8862323a13611c015cb7578e13410a92c52078
Eastar-DS <72285245+Eastar-DS@users.noreply.github.com>
2026-04-06 23:20:48 +0900

    Time: 51 ms (15.11%), Space: 54.6 MB (81.51%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.ts
+++ 0278-first-bad-version/0278-first-bad-version.ts
@@ -8,12 +8,14 @@
 var solution = function(isBadVersion: any) {
 
     return function(n: number): number {
+        // left는 1부터 시작.
         let left = 1;
         let right = n;
 
         while(left < right) {
-            let mid = Math.floor((left + right) / 2);
+            let mid = Math.floor(left + (right - left) / 2);
             if (isBadVersion(mid)) {
+                // right를 mid -1로 두면 mid -1 이 bad가 아닐 경우가 발생. 첫 bad를 찾는데 집중해야함.
                 right = mid;
             } else {
                 left = mid + 1;
